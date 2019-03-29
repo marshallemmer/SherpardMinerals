313265eb07360f59c3bbd9dbe13f403a811312e5
marshallemmer <marshall.emmer15@gmail.com>
2019-03-28 20:08:39 -0500

    Updated navigation bar and imported a icon library to retore functionality Bootstrap abandoned when it went to version 4.

--- ShepardMineralSite/ShepardMineralSite/Shepard.Master.designer.cs
+++ ShepardMineralSite/ShepardMineralSite/Shepard.Master.designer.cs
@@ -22,24 +22,6 @@ namespace ShepardMineralSite {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// btnHome control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnHome;
-        
-        /// <summary>
-        /// btnContact control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnContact;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
